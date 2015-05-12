991830788857513701a7b7618cb953bf52e43608
Rob Allen <rob@akrabat.com>
2015-05-12 09:55:22 +0100

    Add support for PHP's built-in server

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
+
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $file = __DIR__ . $_SERVER['REQUEST_URI'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
