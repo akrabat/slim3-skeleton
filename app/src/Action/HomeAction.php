7d011b27a8b49c68dd87fc483d6f25aa8f801a70
Rob Allen <rob@akrabat.com>
2015-06-23 12:21:56 +0100

    Type hint in HomeAction

--- app/src/Action/HomeAction.php
+++ app/src/Action/HomeAction.php
@@ -1,12 +1,15 @@
 <?php
 namespace App\Action;
 
+use Slim\Views\Twig;
+use Monolog\Logger;
+
 final class HomeAction
 {
     private $view;
     private $logger;
 
-    public function __construct($view, $logger)
+    public function __construct(Twig $view, Logger $logger)
     {
         $this->view = $view;
         $this->logger = $logger;
