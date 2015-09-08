3a2bb6f88c80324bb8fb68a7210a96f522391103
Rob Allen <rob@akrabat.com>
2015-09-08 14:02:38 +0100

    Update to 3.0.0-RC1

--- app/settings.php
+++ app/settings.php
@@ -1,19 +1,20 @@
 <?php
 return [
-
-    // View settings
-    'view' => [
-        'template_path' => __DIR__ . '/templates',
-        'twig' => [
-            'cache' => __DIR__ . '/../cache/twig',
-            'debug' => true,
-            'auto_reload' => true,
+    'settings' => [
+        // View settings
+        'view' => [
+            'template_path' => __DIR__ . '/templates',
+            'twig' => [
+                'cache' => __DIR__ . '/../cache/twig',
+                'debug' => true,
+                'auto_reload' => true,
+            ],
         ],
-    ],
 
-    // monolog settings
-    'logger' => [
-        'name' => 'app',
-        'path' => __DIR__ . '/../log/app.log',
+        // monolog settings
+        'logger' => [
+            'name' => 'app',
+            'path' => __DIR__ . '/../log/app.log',
+        ],
     ],
 ];
