4f28debca2164ed0fb6438a80dafce2abf4cc0de
msheakoski <michael.sheakoski@gmail.com>
2015-09-20 10:41:31 -0400

    Simplify static file check

--- public/index.php
+++ public/index.php
@@ -1,12 +1,9 @@
 <?php
 
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $file = __DIR__ . $_SERVER['REQUEST_URI'];
-    if (is_file($file)) {
-        return false;
-    }
+// To help the built-in PHP dev server, check if the request was actually for
+// something which should probably be served as a static file
+if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
+    return false;
 }
 
 require __DIR__ . '/../vendor/autoload.php';
